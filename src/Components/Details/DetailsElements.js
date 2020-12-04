2d77cd05bed99868fbd33a252ce741229d5083bd
jarekTerejko <j.terejko@gmail.com>
2020-12-04 07:11:39 +0100

    change background for daytime

--- src/Components/Details/DetailsElements.js
+++ src/Components/Details/DetailsElements.js
@@ -2,10 +2,12 @@ import styled from "styled-components";
 
 export const DetailsWrapper = styled.div`
   background-image: unset;
-  background: ${({ hour }) =>
-    hour >= 7 && hour < 19 ? "#CBCBC9" : "rgba(0, 0, 0, .6)"};
-  backdrop-filter: ${({ hour }) =>
-    hour >= 7 && hour < 19 ? "blur(0)" : "blur(5px)"};
+  /* background: ${({ hour }) =>
+    hour >= 7 && hour < 19 ? "#CBCBC9" : "rgba(0, 0, 0, .6)"}; */
+  background: rgba(0, 0, 0, .6);
+  /* backdrop-filter: ${({ hour }) =>
+    hour >= 7 && hour < 19 ? "blur(0)" : "blur(5px)"}; */
+  backdrop-filter:blur(5px);
   min-height: 0;
   height: 0;
   overflow: hidden;
@@ -49,9 +51,9 @@ export const DetailsCol = styled.div`
       right: -1px;
       width: 2px;
       height: 100%;
-      background: ${({ hour }) =>
-        hour >= 7 && hour < 19 ? "#a7a7a7" : "#cacaca"};
-      /* background: #cacaca; */
+      /* background: ${({ hour }) =>
+        hour >= 7 && hour < 19 ? "#a7a7a7" : "#cacaca"}; */
+      background: #cacaca;
       opacity: 0.5;
 
       @media screen and (max-width: 768px) {
@@ -94,7 +96,8 @@ export const DetailsRow = styled.div`
 export const DetailsName = styled.p`
   text-transform: uppercase;
   font-size: 1.4rem;
-  color: ${({ hour }) => (hour >= 7 && hour < 19 ? "#696969" : "#cacaca")};
+  /* color: ${({ hour }) => (hour >= 7 && hour < 19 ? "#696969" : "#cacaca")}; */
+  color: #cacaca;
   margin-bottom: 1.4rem;
   letter-spacing: 1px;
 
@@ -110,7 +113,8 @@ export const DetailsName = styled.p`
 export const DetailsValue = styled.h3`
   font-size: 5rem;
   font-weight: 600;
-  color: ${({ hour }) => (hour >= 7 && hour < 19 ? "#1b1b1b" : "#fafafa")};
+  /* color: ${({ hour }) => (hour >= 7 && hour < 19 ? "#1b1b1b" : "#fafafa")}; */
+  color: #fafafa;
 
   @media screen and (max-width: 950px) {
     font-size: 4rem;
